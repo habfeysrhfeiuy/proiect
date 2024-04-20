cbf7fe9e5e8f06cce5dd065633b59233a5851647
habfeysrhfeiuy <167621309+habfeysrhfeiuy@users.noreply.github.com>
2024-04-20 14:45:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("theme").addEventListener("click", themechanger);
+
+let white = true;
+let black = false;
+
+function themechanger() {
+    console.log("alert");
+    document.querySelector("#test").style.color = "black";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("donate").onclick = function() {}
